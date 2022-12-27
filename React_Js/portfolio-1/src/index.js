7e484cab6a47fc0f2222acf3fc07b6501caf1b63
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-12-27 13:54:46 +0530

    portfolio

---
+++ React_Js/portfolio-1/src/index.js
@@ -0,0 +1,13 @@
+import ReactDom from "react-dom/client"
+import App from './App'
+
+const hw =ReactDom.createRoot(document.getElementById('root')); 
+hw.render(
+    <>
+    <App/>
+    </>
+)
+
+
+
+
